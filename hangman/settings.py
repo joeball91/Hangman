e7dc11799b207a42ec72e297057fec446636306f
joeball91 <joewitkowsky@yahoo.com>
2022-03-11 13:12:57 -0600

    updated settings.py to hide secret key

--- hangman/settings.py
+++ hangman/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&8zbkh&60*fv==(e1x)c1qpeqw3x&r65s+@@mvh!32^14m%e!z'
+SECRET_KEY = env.django_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
