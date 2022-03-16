094139d6c77cde71a4405f1a8f1b2249645ea374
joeball91 <joewitkowsky@yahoo.com>
2022-03-15 21:05:51 -0500

    condensed model with category choices and set up django admin

--- gameplay/models.py
+++ gameplay/models.py
@@ -4,8 +4,22 @@ from django.db import models
 
 
 class Game(models.Model):
-    category = models.CharField(max_length=20)
+
+    ANIMALS = 'ANIMALS'
+    CITIES = 'CITIES'
+    MOVIES = 'MOVIES'
+    CATEGORY_CHOICES = [
+        (ANIMALS, 'Animals'),
+        (CITIES, 'Cities'),
+        (MOVIES, 'Movies'),
+    ]
+
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default=ANIMALS,
+    )
     answer = models.CharField(max_length=20)
     guess = models.CharField(max_length=1)
-    hangman_body = models.CharField(max_length=200)
+    hangman_body = models.ImageField()
     is_game_over = models.BooleanField(default=False)
