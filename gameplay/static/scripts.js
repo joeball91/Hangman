bd250bc9bacc2bd21985004cb135e132435a11f6
joeball91 <joewitkowsky@yahoo.com>
2022-03-31 22:03:09 -0500

    figured out javascript functionality with button!

--- gameplay/static/scripts.js
+++ gameplay/static/scripts.js
@@ -1,1 +1,12 @@
-let const
+function alertButton() {
+    alert('Clicked');
+}
+
+const famous_button = document.getElementById("famous_people_btn");
+const cities_button = document.getElementById("cities_btn");
+const animals_button = document.getElementById("animals_btn");
+
+/* Event listener */
+famous_button.addEventListener("click", alertButton);
+cities_button.addEventListener("click", alertButton);
+animals_button.addEventListener("click", alertButton);
--- gameplay/views.py
+++ gameplay/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from gameplay.models import Game
+
 import random
 
 game_dict = []
@@ -7,6 +9,10 @@ game_dict = []
 def index(request):
     # Home page for Hangman
     return render(request, 'index.html')
+
+
+def get_choice(request):
+    pass
 
 
 def load_dict():
